e7a146e19ccf87b84d74cd53f6bd26fc2726a263
Johannes Besendorfer <johannes.besendorfer@me.com>
2022-05-05 21:26:42 +0200

    add ngModels ERROR

---
+++ src/models/user.class.ts
@@ -0,0 +1,19 @@
+export class User {
+    firstName: string;
+    lastName: string;
+    birthDFate: number; 
+    street: string;
+    zipCode: number;
+    city: string;
+
+    constructor(obj?: any) {
+    
+        this.firstName = obj ? obj.firstName : '';
+        this.lastName = obj ? obj.lastName : '';
+        this.birthDFate = obj ? obj.birthDFate : '';
+        this.street = obj ? obj.street : '';
+        this.zipCode = obj ? obj.zipCode : '';
+        this.city = obj ? obj.city : '';
+ 
+    }
+}
